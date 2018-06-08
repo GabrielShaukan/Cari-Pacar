25fc63c7404e0811b52ff72a38a5d7e9db1ab060
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-08 18:50:51 +0200

    Implemented getUserInfo() function

--- app/src/main/java/com/shaukan/gabriel/tinderclone/SettingsActivity.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.shaukan.gabriel.tinderclone;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +10,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,10 +52,36 @@ public class SettingsActivity extends AppCompatActivity {
         userId = mAuth.getCurrentUser().getUid();
         mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userId);
 
+        getUserInfo();
+
         mConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 saveUserInformation();
+            }
+        });
+    }
+
+    private void getUserInfo() {
+        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object > map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name") != null) {
+                        name = map.get("name").toString();
+                        mNameField.setText(name);
+                    }
+
+                    if(map.get("name") != null) {
+                        name = map.get("phone").toString();
+                        mNameField.setText(phone);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
