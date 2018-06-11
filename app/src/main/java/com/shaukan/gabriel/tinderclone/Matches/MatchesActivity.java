04337a9b3725598c3d2161c62e20472a6d41811d
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-11 18:03:58 +0200

    Implemented functions that gets matches IDs

--- app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesActivity.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesActivity.java
@@ -1,10 +1,17 @@
 package com.shaukan.gabriel.tinderclone.Matches;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.shaukan.gabriel.tinderclone.R;
 
 import java.util.ArrayList;
@@ -14,11 +21,14 @@ public class MatchesActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mMatchesAdapter;
     private RecyclerView.LayoutManager mMatchesLayoutManager;
+    private String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
+
+        currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
@@ -28,6 +38,8 @@ public class MatchesActivity extends AppCompatActivity {
         mMatchesAdapter = new MatchesAdapter(getDataSetMatches(), MatchesActivity.this);
         mRecyclerView.setAdapter(mMatchesAdapter);
 
+        getUserMatchId();
+
         for (int i=0;i<100;i++) {
             MatchesObject obj = new MatchesObject(Integer.toString(i));
             resultsMatches.add(obj);
@@ -36,6 +48,29 @@ public class MatchesActivity extends AppCompatActivity {
         mMatchesAdapter.notifyDataSetChanged();
     }
 
+    private void getUserMatchId() {
+
+        DatabaseReference matchDb = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("connections").child("matches");
+        matchDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for(DataSnapshot match : dataSnapshot.getChildren()) {
+                        FetchMatchInformation(match.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void FetchMatchInformation(String key) {
+        
+    }
 
     private ArrayList<MatchesObject> resultsMatches = new ArrayList<MatchesObject>();
     private List<MatchesObject> getDataSetMatches() {
