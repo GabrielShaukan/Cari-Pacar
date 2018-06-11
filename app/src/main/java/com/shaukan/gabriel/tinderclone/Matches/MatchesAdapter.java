439cf00b78703faee6a20b1bcdec14a7ac4f2073
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-11 17:27:22 +0200

    Created Matches Adapter

---
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesAdapter.java
@@ -0,0 +1,46 @@
+package com.shaukan.gabriel.tinderclone.Matches;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shaukan.gabriel.tinderclone.R;
+
+import java.util.List;
+
+public class MatchesAdapter extends RecyclerView.Adapter<MatchesViewHolder> {
+    private List<MatchesObject> matchesList;
+    private Context context;
+
+    public MatchesAdapter(List<MatchesObject> matchesList, Context context {
+        this.matchesList = matchesList;
+        this.context = context;
+    };
+
+    @NonNull
+    @Override
+    public MatchesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_matches, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutView.setLayoutParams(lp);
+        MatchesViewHolder rcv = new MatchesViewHolder((layoutView));
+
+
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MatchesViewHolder holder, int position) {
+        holder.mMatchId.setText(matchesList.get(position).getUserId());
+        
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
