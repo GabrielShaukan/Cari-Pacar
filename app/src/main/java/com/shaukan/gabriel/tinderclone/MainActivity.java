cc4c57708690a668901d54dd3cb21de118b0b852
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-11 19:58:42 +0200

    Coded Messaging functions for database

--- app/src/main/java/com/shaukan/gabriel/tinderclone/MainActivity.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/MainActivity.java
@@ -115,8 +115,11 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     Toast.makeText(MainActivity.this, "New Connection", Toast.LENGTH_LONG).show();
-                    usersDb.child(dataSnapshot.getKey()).child("connections").child("matches").child(currentUId).setValue(true);
-                    usersDb.child(currentUId).child("connections").child("matches").child(dataSnapshot.getKey()).setValue(true);
+
+                    String key = FirebaseDatabase.getInstance().getReference().child("Chat").push().getKey();
+
+                    usersDb.child(dataSnapshot.getKey()).child("connections").child("matches").child(currentUId).child("ChatId").setValue(key);
+                    usersDb.child(currentUId).child("connections").child("matches").child(dataSnapshot.getKey()).child("ChatId").setValue(key);
                 }
             }
 
