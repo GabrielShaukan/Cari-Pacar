4d09f5b48b8b6d251901cf2a2ddcc3972c9fc4a9
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-11 17:08:41 +0200

    Created Matches View Holder

--- app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesViewHolder.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesViewHolder.java
@@ -1,4 +1,22 @@
 package com.shaukan.gabriel.tinderclone.Matches;
 
-public class MatchesViewHolder {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.shaukan.gabriel.tinderclone.R;
+
+public class MatchesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView mMatchId;
+    public MatchesViewHolder(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
