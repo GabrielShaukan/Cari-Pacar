bbc1719b20e507cbff62eec5276a77003fa30878
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-11 17:56:08 +0200

    Updated ViewHolder

--- app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesViewHolder.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/Matches/MatchesViewHolder.java
@@ -2,17 +2,22 @@ package com.shaukan.gabriel.tinderclone.Matches;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shaukan.gabriel.tinderclone.R;
 
 public class MatchesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    public TextView mMatchId;
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+
     public MatchesViewHolder(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
 
         mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
     }
 
     @Override
