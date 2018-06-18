c61b519c12917f676572bcbd71455d88dc32d52c
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-18 14:51:18 +0200

    Changed startup activity when user already logged in

--- app/src/main/java/com/shaukan/gabriel/tinderclone/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/ChooseLoginRegistrationActivity.java
@@ -1,18 +1,39 @@
 package com.shaukan.gabriel.tinderclone;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     private Button mLogin, mRegister;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
 
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
@@ -36,5 +57,17 @@ public class ChooseLoginRegistrationActivity extends AppCompatActivity {
                 return;
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthStateListener);
     }
 }
