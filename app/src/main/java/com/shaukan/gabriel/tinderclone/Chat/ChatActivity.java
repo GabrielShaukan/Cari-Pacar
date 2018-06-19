a397335547af416acf541276892987faf607daef
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-06-19 22:27:48 +0200

    Implemented auto scroll feature in chat

--- app/src/main/java/com/shaukan/gabriel/tinderclone/Chat/ChatActivity.java
+++ app/src/main/java/com/shaukan/gabriel/tinderclone/Chat/ChatActivity.java
@@ -2,6 +2,7 @@ package com.shaukan.gabriel.tinderclone.Chat;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -133,6 +135,15 @@ public class ChatActivity extends AppCompatActivity {
 
                     }
                 }
+
+                final NestedScrollView scrollview = ((NestedScrollView) findViewById(R.id.scrollView));
+                scrollview.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollview.fullScroll(ScrollView.FOCUS_DOWN);
+                    }
+                });
+
             }
 
             @Override
